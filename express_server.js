d81e6f32d8f05f6b4bb63e74352d711d06a73cca
Hoss Agina <hoss.agina@telus.com>
2022-11-30 14:15:00 -0700

    made sure that user's email displays after reading cookie

--- express_server.js
+++ express_server.js
@@ -46,14 +46,13 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  // const userObj = users[req.cookies["user_id"]]
   const templateVars = { user: users[req.cookies["user_id"]], urls: urlDatabase };
   res.render("urls_index", templateVars);
  });
 
 
  app.get("/urls/new", (req, res) => {
-  const templateVars = { user: req.cookies["user_id"] }
+  const templateVars = { user: users[req.cookies["user_id"]] }
   res.render("urls_new", templateVars);
 });
 
@@ -64,7 +63,7 @@ app.post("/urls", (req, res) => {
 });
 
  app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: req.cookies["user_id"] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]] };
   if (urlDatabase[req.params.id]) {
     res.render("urls_show", templateVars);
   } else {
