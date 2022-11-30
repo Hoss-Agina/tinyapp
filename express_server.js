ad68e170c4218866e15928f9692dec7692a82085
Hoss Agina <hoss.agina@telus.com>
2022-11-29 21:55:56 -0700

    added login and logout capabilities to tinyApp

--- express_server.js
+++ express_server.js
@@ -34,13 +34,14 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
  });
 
 
  app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -50,7 +51,7 @@ app.post("/urls", (req, res) => {
 });
 
  app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   if (urlDatabase[req.params.id]) {
     res.render("urls_show", templateVars);
   } else {
@@ -81,6 +82,11 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
